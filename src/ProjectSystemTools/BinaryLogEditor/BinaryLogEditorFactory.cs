0b880dd444cb99fb66201dabc0b01227e22ec433
Paul Vick <paulv@microsoft.com>
2017-09-01 15:45:10 -0700

    More changes

--- src/ProjectSystemTools/BinaryLogEditor/BinaryLogEditorFactory.cs
+++ src/ProjectSystemTools/BinaryLogEditor/BinaryLogEditorFactory.cs
@@ -3,14 +3,69 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using IServiceProvider = System.IServiceProvider;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Tools.BinaryLogEditor
 {
     [Guid(ProjectSystemToolsPackage.BinaryLogEditorFactoryGuidString)]
     public sealed class BinaryLogEditorFactory : IVsEditorFactory, IVsEditorFactory2, IVsEditorFactory3
     {
+        private static readonly Guid IID_IUnknown = new Guid("{00000000-0000-0000-C000-000000000046}");
+        private static readonly Guid TextEditorCmdUIGuid = new Guid(0x8B382828, 0x6202, 0x11d1, 0x88, 0x70, 0x00, 0x00, 0xF8, 0x75, 0x79, 0xD2);
+
+        private const int CLSCTX_INPROC_SERVER = 0x1;
+
         private OLE.Interop.IServiceProvider _site;
+        private ServiceProvider _serviceProvider;
+
+        private object CreateView(IVsHierarchy hierarchy, uint itemid, string fileName, object docData, out Guid cmdUIGuid)
+        {
+            cmdUIGuid = TextEditorCmdUIGuid;
+            var guidCodeWindow = typeof(IVsCodeWindow).GUID;
+            IVsCodeWindow codeWindow;
+
+            if (!(_serviceProvider.GetService(typeof(ILocalRegistry)) is ILocalRegistry localRegistry))
+            {
+                throw new InvalidOperationException();
+            }
+
+            ErrorHandler.ThrowOnFailure(localRegistry.CreateInstance(
+                typeof(VsCodeWindowClass).GUID,
+                null,
+                ref guidCodeWindow,
+                CLSCTX_INPROC_SERVER,
+                out var codeWindowPtr)
+            );
+
+            try
+            {
+                codeWindow = (IVsCodeWindow)Marshal.GetObjectForIUnknown(codeWindowPtr);
+            }
+            finally
+            {
+                Marshal.Release(codeWindowPtr);
+            }
+
+            var lines = docData as IVsTextLines;
+            if (lines == null)
+            {
+                if (docData is IVsTextBufferProvider provider)
+                {
+                    ErrorHandler.ThrowOnFailure(provider.GetTextBuffer(out lines));
+                }
+            }
+
+            if (lines != null)
+            {
+                ErrorHandler.ThrowOnFailure(codeWindow.SetBuffer(lines));
+            }
+
+            return codeWindow;
+        }
 
         int IVsEditorFactory.CreateEditorInstance(uint vsCreateEditorFlags, string fileName, string physicalView,
             IVsHierarchy hierarchy, uint itemid, IntPtr existingDocData,
@@ -34,6 +89,55 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools.BinaryLogEditor
                     throw new ArgumentException(Resources.BadCreateFlags, nameof(vsCreateEditorFlags));
                 }
 
+                object docDataObject;
+
+                if (existingDocData == IntPtr.Zero)
+                {
+                    var localRegistry = _serviceProvider.GetService(typeof(ILocalRegistry)) as ILocalRegistry;
+                    if (localRegistry == null)
+                    {
+                        throw new InvalidOperationException();
+                    }
+
+                    var guidUnknown = IID_IUnknown;
+                    ErrorHandler.ThrowOnFailure(localRegistry.CreateInstance(
+                                                typeof(VsTextBufferClass).GUID,
+                                                null,
+                                                ref guidUnknown,
+                                                CLSCTX_INPROC_SERVER,
+                                                out var newDocDataPtr)
+                    );
+
+                    try
+                    {
+                        docDataObject = Marshal.GetObjectForIUnknown(newDocDataPtr);
+                    }
+                    finally
+                    {
+                        Marshal.Release(newDocDataPtr);
+                    }
+
+                    if (docDataObject is IObjectWithSite ows)
+                    {
+                        ows.SetSite(_site);
+                    }
+                }
+                else
+                {
+                    docDataObject = Marshal.GetObjectForIUnknown(existingDocData);
+                    if (!(docDataObject is IVsTextLines textLines))
+                    {
+                        // Need to close first
+                        return VSConstants.VS_E_INCOMPATIBLEDOCDATA;
+                    }
+                }
+
+                view = CreateView(hierarchy, itemid, fileName, docDataObject, out cmdUIGuid);
+
+                caption = "foo";
+                docView = Marshal.GetIUnknownForObject(view);
+                docData = Marshal.GetIUnknownForObject(docDataObject);
+                view = null;
             }
             finally
             {
@@ -52,11 +156,17 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools.BinaryLogEditor
         int IVsEditorFactory.SetSite(OLE.Interop.IServiceProvider site)
         {
             _site = site;
+            _serviceProvider = new ServiceProvider(_site, false);
             return VSConstants.S_OK;
         }
 
         int IVsEditorFactory.Close()
         {
+            if (_serviceProvider != null)
+            {
+                _serviceProvider.Dispose();
+                _serviceProvider = null;
+            }
             _site = null;
             return VSConstants.S_OK;
         }
