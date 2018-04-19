42545565a1d9baab90eb3110dd50adccb7ad730b
Tom Meschter <tom.meschter@microsoft.com>
2018-04-19 13:18:28 -0700

    Add support for querying properties and output groups
    This change adds support for querying MSBuild properties and output groups. A couple of new menu items, "Get MSBuild Property..." and "Get Output Group...", have been added to the Tools menu. Selecting one of these pops up a dialog box where the name of a property or output group can be entered. The results are then displayed in a dedicated pane in the Output Window.
    
    In the current implementation we query each project in the solution, rather than having the user pick one. Querying for MSBuild properties goes through the `IVsBuildPropertyStorage` interface, and the output groups are retrieved through the `EnvDTE.ConfigurationManager` interface.

---
+++ src/ProjectSystemTools/ProjectQuery/ProjectQueryDialog.xaml.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Windows;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tools.ProjectQuery
+{
+    /// <summary>
+    /// Interaction logic for ProjectQueryDialog.xaml
+    /// </summary>
+    internal partial class ProjectQueryDialog : DialogWindow
+    {
+        public ProjectQueryDialog(string title, string message)
+        {
+            InitializeComponent();
+
+            Title = title;
+            MessageBlock.Text = message;
+
+            InputField.Focus();
+        }
+
+        private void OnOKButton(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void OnCancelButton(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
--- src/ProjectSystemTools/ProjectSystemToolsPackage.cs
+++ src/ProjectSystemTools/ProjectSystemToolsPackage.cs
@@ -1,19 +1,22 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Microsoft.VisualStudio.Shell;
-using Task = System.Threading.Tasks.Task;
-using System.ComponentModel.Design;
+using EnvDTE;
+using EnvDTE80;
 using Microsoft.Internal.VisualStudio.Shell.TableControl;
 using Microsoft.VisualStudio.ComponentModelHost;
 using Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogExplorer;
 using Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging;
+using Microsoft.VisualStudio.ProjectSystem.Tools.ProjectQuery;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Shell.TableManager;
-using Microsoft.VisualStudio.Threading;
 using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Threading;
+using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Tools
 {
@@ -42,6 +45,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools
         public const int ExploreLogsCommandId = 0x0109;
         public const int AddLogCommandId = 0x010a;
         public const int MessageListCommandId = 0x010b;
+        public const int QueryMSBuildPropertyCommandId = 0x010c;
+        public const int QueryOutputGroupCommandId = 0x010d;
 
         public static readonly Guid UIGuid = new Guid("629080DF-2A44-40E5-9AF4-371D4B727D16");
 
@@ -53,14 +58,20 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools
         private BuildLoggingToolWindow _buildLoggingToolWindow;
         private BuildLogExplorerToolWindow _buildLogExplorerToolWindow;
 
+        private OutputWindowPane _projectQueryOutputWindowPane;
+
         public static IServiceProvider ServiceProvider { get; private set; }
 
         public BuildLoggingToolWindow BuildLoggingToolWindow => _buildLoggingToolWindow ?? (_buildLoggingToolWindow = (BuildLoggingToolWindow)FindToolWindow(typeof(BuildLoggingToolWindow), 0, true));
         public BuildLogExplorerToolWindow BuildLogExplorerToolWindow => _buildLogExplorerToolWindow ?? (_buildLogExplorerToolWindow = (BuildLogExplorerToolWindow)FindToolWindow(typeof(BuildLogExplorerToolWindow), 0, true));
 
+        public OutputWindowPane ProjectQueryOutputPane => _projectQueryOutputWindowPane ?? (_projectQueryOutputWindowPane = (DTE.ToolWindows.OutputWindow.OutputWindowPanes.Add("Project Query")));
+
         public static IVsUIShell VsUIShell { get; private set; }
 
         public static IVsFindManager VsFindManager { get; private set; }
+
+        private static DTE2 DTE { get; set; }
 
         public static ProjectSystemToolsPackage Instance;
 
@@ -83,6 +94,7 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools
 
             VsUIShell = GetService(typeof(IVsUIShell)) as IVsUIShell;
             VsFindManager = GetService(typeof(SVsFindManager)) as IVsFindManager;
+            DTE = GetService(typeof(SDTE)) as DTE2;
 
             var componentModel = GetService(typeof(SComponentModel)) as IComponentModel;
             TableControlProvider = componentModel?.GetService<IWpfTableControlProvider>();
@@ -92,6 +104,8 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools
             mcs?.AddCommand(new MenuCommand(ShowBuildLoggingToolWindow, new CommandID(CommandSetGuid, BuildLoggingCommandId)));
             mcs?.AddCommand(new MenuCommand(ShowBuildLogExplorerToolWindow, new CommandID(CommandSetGuid, BuildLogExplorerCommandId)));
             mcs?.AddCommand(new MenuCommand(ShowMessageListToolWindow, new CommandID(CommandSetGuid, MessageListCommandId)));
+            mcs?.AddCommand(new MenuCommand(QueryMSBuildProperty, new CommandID(CommandSetGuid, QueryMSBuildPropertyCommandId)));
+            mcs?.AddCommand(new MenuCommand(QueryOutputGroup, new CommandID(CommandSetGuid, QueryOutputGroupCommandId)));
 
             Instance = this;
         }
@@ -136,6 +150,106 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools
 
             var windowFrame = (IVsWindowFrame)window.Frame;
             ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+
+        private void QueryMSBuildProperty(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dialog = new ProjectQueryDialog("Get MSBuild Property", "Retrieves the value of an MSBuild property via the project's IVsBuildPropertyStorage implementation.");
+
+            var retrieveProperty = dialog.ShowModal();
+            if (!retrieveProperty.HasValue
+                || !retrieveProperty.Value)
+            {
+                return;
+            }
+
+            ProjectQueryOutputPane.Activate();
+            ShowOutputWindow();
+
+            var propertyName = dialog.InputField.Text;
+
+            var solution = GetService(typeof(SVsSolution)) as IVsSolution;
+            if (solution == null)
+            {
+                return;
+            }
+
+            var guid = Guid.Empty;
+            if (solution.GetProjectEnum((uint)__VSENUMPROJFLAGS.EPF_LOADEDINSOLUTION, ref guid, out IEnumHierarchies hierarchies) == VSConstants.S_OK)
+            {
+                var hierarchyArray = new IVsHierarchy[1];
+                while (hierarchies.Next((uint)hierarchyArray.Length, hierarchyArray, out uint fetched) == 0
+                        && fetched > 0)
+                {
+                    if (hierarchyArray[0] is IVsBuildPropertyStorage buildPropertyStorage
+                        && buildPropertyStorage.GetPropertyValue(propertyName, null, (uint)_PersistStorageType.PST_PROJECT_FILE, out string propertyValue) == VSConstants.S_OK
+                        && hierarchyArray[0].GetProperty((uint)VSConstants.VSITEMID.Root, (int)__VSHPROPID.VSHPROPID_Name, out object projectNameObject) == VSConstants.S_OK)
+                    {
+                        var projectName = (string)projectNameObject;
+                        ProjectQueryOutputPane.OutputString($"$({propertyName}) in {projectName} = {propertyValue}\r\n");
+                    }
+                }
+            }
+        }
+
+        private void QueryOutputGroup(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var dialog = new ProjectQueryDialog("Get Output Group", "Retrieves the list of items in the specified output group.");
+
+            var retrieveOutputGroup = dialog.ShowModal();
+            if (!retrieveOutputGroup.HasValue
+                || !retrieveOutputGroup.Value)
+            {
+                return;
+            }
+
+            ProjectQueryOutputPane.Activate();
+            ShowOutputWindow();
+
+            var outputGroupName = dialog.InputField.Text;
+            foreach (Project project in DTE.Solution.Projects)
+            {
+                var configurationManager = project.ConfigurationManager;
+                if (configurationManager == null)
+                {
+                    return;
+                }
+
+                var activeConfiguration = configurationManager.ActiveConfiguration;
+                if (activeConfiguration == null)
+                {
+                    return;
+                }
+
+                var outputGroups = activeConfiguration.OutputGroups;
+                if (outputGroups == null)
+                {
+                    return;
+                }
+
+                var outputGroup = outputGroups.Item(outputGroupName);
+                if (outputGroup == null)
+                {
+                    return;
+                }
+
+                ProjectQueryOutputPane.OutputString($"Output group '{outputGroupName}' in project {project.Name}:\r\n");
+                Array fileNames = (Array)outputGroup.FileNames;
+                foreach (string fileName in fileNames)
+                {
+                    ProjectQueryOutputPane.OutputString($"  {fileName}\r\n");
+                }
+            };
+        }
+
+        private void ShowOutputWindow()
+        {
+            var outputWindow = DTE.Windows.Item(EnvDTE.Constants.vsWindowKindOutput);
+            outputWindow.Activate();
         }
     }
 }
