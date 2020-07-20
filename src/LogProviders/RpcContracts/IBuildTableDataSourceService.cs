232e1d97883ba78d691a79b35bf69837e8120e68
Eric Yoon <t-eriyoo@microsoft.com>
2020-07-20 09:59:40 -0700

    starting interface with Codespaces

---
+++ src/LogProviders/RpcContracts/IBuildTableDataSourceService.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.ProjectSystem.LogModel;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Tools.Providers.RpcContracts
+{
+    public interface IBuildTableDataSourceService
+    {
+        Task<bool> IsLogging { get; }
+
+        Task<bool> Start();  // Should this be void return type?
+
+        Task<bool> Stop();  // Should this be void return type?
+
+        Task<bool> Clear();  // Would it be a good idea to have this? Technically the client could probably handle that logic and just clear the UI
+
+        Task<BuildHandle> NotifyBuildStart();
+
+        Task<bool> NotifyBuildFinished();  // Should this be void return type?
+
+        Task<Log> RetrieveLogForBuild();
+
+    }
+}
