20601a949dbd93f3e8f05c2a247e73811b16fe4a
Eric Yoon <t-eriyoo@microsoft.com>
2020-07-21 11:42:51 -0700

    Add basic documentation and remove unnecessary APIs

--- src/LogProviders/RpcContracts/IBuildTableDataSourceService.cs
+++ src/LogProviders/RpcContracts/IBuildTableDataSourceService.cs
@@ -7,20 +7,54 @@ namespace Microsoft.VisualStudio.ProjectSystem.Tools.Providers.RpcContracts
 {
     public interface IBuildTableDataSourceService
     {
+        /// <summary>
+        /// Returns whether or not the build logging window is currently tracking logs or not
+        /// </summary>
+        /// <returns>True if build logging window is tracking logs and false otherwise</returns>
         Task<bool> IsLogging();
 
+        /// <summary>
+        /// Tell build logging to start tracking logs
+        /// </summary>
+        /// <returns>True is operation succeeded and false if not</returns>
         Task<bool> Start();  // Should this be void return type?
 
+        /// <summary>
+        /// Tell build logging to stop tracking logs
+        /// </summary>
+        /// <returns>True if operation succeeded and false if not</returns>
         Task<bool> Stop();  // Should this be void return type?
 
+        /// <summary>
+        /// Tell build logging to clear out all the accumulated logs from the build logging window
+        /// </summary>
+        /// <returns>True if operation succeeded and false if not</returns>
         Task<bool> Clear();  // Would it be a good idea to have this? Technically the client could probably handle that logic and just clear the UI
 
+        /// <summary>
+        /// Send a notification through that a build has started
+        /// </summary>
+        /// <returns>A handle that the user can use to retrieve the given build</returns>
         Task<BuildHandle> NotifyBuildStart();
 
+        /// <summary>
+        /// Send a notification through that a build has finished
+        /// </summary>
+        /// <returns>True if operation succeeded and false if not</returns>
         Task<bool> NotifyBuildFinished();  // Should this be void return type?
 
+        /// <summary>
+        /// Gives the user a log of a requested build
+        /// </summary>
+        /// <param name="handle">an ID (type BuildHandle) used to retrieve a unique log for a build</param>
+        /// <returns>The log tied to the requested BuildHandle</returns>
         Task<Log> RetrieveLogForBuild(BuildHandle handle);
 
-        Task<bool> SaveBuildLogToServer();  // Would it be a good idea to have this?
+        /// <summary>
+        /// Gives the user a requested build
+        /// </summary>
+        /// <param name="handle">an ID (type BuildHandle) used to retrieve a unique build</param>
+        /// <returns>The Build tied to the requested BuildHandle</returns>
+        Task<Build> RetrieveBuild(BuildHandle handle);
     }
 }
