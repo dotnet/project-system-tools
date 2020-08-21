5e79a66b355791e2712d4840d4fa15199276316b
Eric Yoon <t-eriyoo@microsoft.com>
2020-08-21 12:00:30 -0700

    Resolving editor errors

--- src/LogProviders/BackEnd/RoslynLogger.cs
+++ src/LogProviders/BackEnd/RoslynLogger.cs
@@ -6,6 +6,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging;
+using Microsoft.VisualStudio.ProjectSystem.Tools.Providers;
 
 namespace Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging.Model.BackEnd
 {
