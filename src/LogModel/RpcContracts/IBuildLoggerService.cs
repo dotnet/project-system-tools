76f54f07084adf160563053b94af9610000b2fd6
Eric Yoon <t-eriyoo@microsoft.com>
2020-09-01 12:17:05 -0700

    add nullable enable

--- src/LogModel/RpcContracts/IBuildLoggerService.cs
+++ src/LogModel/RpcContracts/IBuildLoggerService.cs
@@ -5,6 +5,8 @@ using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 
+#nullable enable
+
 namespace Microsoft.VisualStudio.ProjectSystem.Tools.BuildLogging.Model.RpcContracts
 {
     /// <summary>
