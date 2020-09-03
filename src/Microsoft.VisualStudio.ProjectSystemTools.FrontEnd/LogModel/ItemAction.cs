d1a54525f96f24de6a0a6dcc361791c17b3c8348
Eric Yoon <t-eriyoo@microsoft.com>
2020-09-03 10:56:23 -0700

    Renaming src folders to match project names

---
+++ src/Microsoft.VisualStudio.ProjectSystemTools.FrontEnd/LogModel/ItemAction.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.VisualStudio.ProjectSystemTools.FrontEnd
+{
+    public sealed class ItemAction
+    {
+        public bool IsAddition { get; }
+        public ItemGroup ItemGroup { get; }
+        public DateTime Time { get; }
+
+        public ItemAction(bool isAddition, ItemGroup itemGroup, DateTime time)
+        {
+            IsAddition = isAddition;
+            ItemGroup = itemGroup;
+            Time = time;
+        }
+    }
+}
---
+++ src/Microsoft.VisualStudio.ProjectSystemTools.FrontEnd/LogModel/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Microsoft.VisualStudio.ProjectSystemTools.FrontEnd
+{
+    public class Message
+    {
+        public DateTime Timestamp { get; }
+        public string Text { get; }
+
+        public Message(DateTime timestamp, string text)
+        {
+            Timestamp = timestamp;
+            Text = text;
+        }
+    }
+}
