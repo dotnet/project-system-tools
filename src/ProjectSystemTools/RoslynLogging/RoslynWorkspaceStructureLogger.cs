5be27fb0010c4d9edf85257d14b6f875db4db6a1
Drew Noakes <git@drewnoakes.com>
2021-08-05 20:41:48 +1000

    Use MIT license
    This commit changes the repository's license from Apache 2.0 to Apache.

--- src/ProjectSystemTools/RoslynLogging/RoslynWorkspaceStructureLogger.cs
+++ src/ProjectSystemTools/RoslynLogging/RoslynWorkspaceStructureLogger.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All Rights Reserved. Licensed under the MIT license. See the LICENSE.md file in the project root for more information.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
